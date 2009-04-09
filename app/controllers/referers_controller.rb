cc379c1d8ebbbf94d39ff366d16f74c280e09907
rosario <sarhus@gmail.com>
2009-04-09 16:00:59 +0100

    First partial render for referers implemented, Referers now shows Searched Terms and Websites

--- app/controllers/referers_controller.rb
+++ app/controllers/referers_controller.rb
@@ -17,9 +17,9 @@ class ReferersController < ApplicationController
     
     
     
-     @queries = p.visitors.count(:id, 
-                :group => 'referer_keyword', 
-                :conditions => conditions).sort_by{|x,y| y}.reverse
+     # @queries = p.visitors.count(:id, 
+     #                :group => 'referer_keyword', 
+     #                :conditions => conditions).sort_by{|x,y| y}.reverse
                 
     total_visits = p.visitors.find(:all, :conditions => conditions)
     
@@ -45,9 +45,51 @@ class ReferersController < ApplicationController
 
     end
     
-    @data = list.sort_by{|q,v,p,t| v}.reverse.first(15)
+    @data = list.sort_by{|q,v,a,t| v}.reverse.first(25)
     
+     ##################
+     
+     # Website
+      conditions2 = ["referer_type = ? AND created_at > ? AND created_at < ? and referer_name NOT NULL", 
+                     "3", "#{bod}", "#{eod}"]
+
+      # p =Project.find(session[:project_id])
       
+      
+      total_visits = p.visitors.find(:all, :conditions => conditions2)
+      
+      
+      
+        queries = total_visits.group_by{|v| v.referer_name}
+
+        list = []
+
+
+        queries.keys.each do |k|
+          vs = queries[k]
+          nvisitors = queries[k].size
+
+          total_actions = vs.inject(0){|total,v| total + v.total_actions}
+          total_time = vs.inject(0){|total,v| total + v.time_spent}
+          avg_time = (total_time/nvisitors).to_i
+          avg_time = [avg_time/60 % 60, avg_time % 60].map{|t| t.to_s.rjust(2,'0')}.join(':')
+          list << [k,
+            nvisitors,
+            "%.2f" % ((total_actions+0.0)/nvisitors),
+            avg_time
+
+          ]
+
+        end
+      
+        @data2 = list.sort_by{|q,v,p,t| v}.reverse.first(25)
+        
+        #############
+        
+        # conditions3 = ["referer_type = ? AND created_at > ? AND created_at < ?", 
+        #                         "2", "#{bod}", "#{eod}"]
+        #  
+        #                         total_visits = p.visitors.find(:all, :conditions => conditions3)
   
     
   end
