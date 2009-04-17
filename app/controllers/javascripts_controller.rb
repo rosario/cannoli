8da12753f81a1ba0cabb0ce2e9581cb6ec770a34
rosario <sarhus@gmail.com>
2009-04-17 10:08:16 +0100

    Check if the project_id sent in the params[:url] matches with the correct project_id

--- app/controllers/javascripts_controller.rb
+++ app/controllers/javascripts_controller.rb
@@ -2,7 +2,11 @@
 
 class JavascriptsController < ApplicationController
 
-
+  def hostname_check
+ 
+    
+    
+  end
 
   # The main javascript tracker function, at the moment it's basically
   # taken from piwik.org. It registers the visitor informations and pass it
@@ -16,6 +20,7 @@ class JavascriptsController < ApplicationController
   
   def log
       
+ 
       
       if session[:user_id]
         # User (not the visitor) is logged in..we dont count his actions
@@ -23,45 +28,39 @@ class JavascriptsController < ApplicationController
         render :text => ""
       else
         
-           project_id = params[:idsite]
+        project_id = params[:idsite]
 
-            if (session[:project_id]) and (project_id != session[:project_id])
-              # This visitor has already a session open, but using another project
-              session[:visitor_id] = nil
+        if (session[:project_id]) and (project_id != session[:project_id])
+        # This visitor has already a session open, but using another project
+          session[:visitor_id] = nil
+        end
+
+        session[:project_id] = project_id
+
+
+        p = Project.find(project_id) 
+
+        # Check if the params[:idsite] correspond to the right hostname
+        # because we dont want to record stats from other hosts
+
+        hostname = URI.parse(params[:url]).host
+        if hostname.match(p.name) 
+          puts " >>>>>>>>>>>>>>>>>> HOSTNAME MATCHES with the correct project_id"
+          visitor_settings = Tracker.get_settings(params,request)  
+          if session[:visitor_id]
+            # Se c''e il visitor_id allora e' conosciuto
+            # Vedo se l'ultima azione e' stata fatta in meno di mezzo'ora
+            puts " >>>>>>>>>>>>>>>>> SESSION FOUND"
+            v = Visitor.find(session[:visitor_id])
+            if (Time.now - v.last_action_time) > 30.minutes
+              # Sono passati piu' di 30 minuti, creo un nuovo visitor e lo metto come returning
+              puts ">>>>>>>>>>>>>>>>>>>MORE THAN 30 minutes, create a new visitor"
+              v = Visitor.create_with_settings(visitor_settings)
+              p.visitors << v
+              p.visitor_was_here!(v)
             end
 
-            session[:project_id] = project_id
-
-
-            p = Project.find(project_id) 
-
-
-            # Check if the params[:idsite] correspond to the right hostname
-            # because we dont want to record stats from other hosts
-            # Fix this code..it needs to work for www.hostname.com and hostname.com
-            # hostname = URI.parse(params[:url]).host
-            #       if p.name != hostname 
-            #         render :text => ""
-            #       end
-            #       
-            #       
-
-           visitor_settings = Tracker.get_settings(params,request)  
-            if session[:visitor_id]
-              # faccio qualche cosa
-              # Se c''e il visitor_id allora e' conosciuto
-              # Vedo se l'ultima azione e' stata fatta in meno di mezzo'ora
-              puts " >>>>>>>>>>>>>>>>> SESSION FOUND"
-              v = Visitor.find(session[:visitor_id])
-                if (Time.now - v.last_action_time) > 30.minutes
-                  # Sono passati piu' di 30 minuti, creo un nuovo visitor e lo metto come returning
-                  puts ">>>>>>>>>>>>>>>>>>>MORE THAN 30 minutes, create a new visitor"
-                  v = Visitor.create_with_settings(visitor_settings)
-                  p.visitors << v
-                  p.visitor_was_here!(v)
-                end
-
-            else
+          else
 
               puts ">>>>>>>>>>>>>>>>> NO SESSION"
               # There's no session, check if there was a visitor having the same browser configuration and IP today
@@ -86,29 +85,30 @@ class JavascriptsController < ApplicationController
 
               end
 
-            end
+          end
 
-            session[:visitor_id] = v.id
+          session[:visitor_id] = v.id
 
 
-            # Save the action only IF it's not already present.
-            # Kind is used to distinguish between normal actions and goals
-            # Other kinds should be used too, (such as downloads)
+          # Save the action only IF it's not already present.
+          # Kind is used to distinguish between normal actions and goals
+          # Other kinds should be used too, (such as downloads)
 
-            # Clean the URL from the last '/'
-            url = params[:url]
-            if url.last == "/"
-              url.chop!
-            end
+          # Clean the URL from the last '/'
+          url = params[:url]
+          if url.last == "/"
+            url.chop!
+          end
 
-            action = Action.new(:url=>"#{url}", :url_id=> Digest::MD5.hexdigest(params[:url]),
+          action = Action.new(:url=>"#{url}", :url_id=> Digest::MD5.hexdigest(params[:url]),
                                    :kind => params[:action_kind].to_i) 
           # Add the action to the project, only if it's new
           # Return the old action if present
-           a = p.add_action(action)  
+          a = p.add_action(action)  
 
-           v.add_action(a)
-        
+          v.add_action(a)
+        end
+           
         
         
       end
